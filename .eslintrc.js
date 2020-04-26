871f463acfff7f6a071a5f1757a1f8a788a4a138
tunahanoguz <tunahanog@gmail.com>
2020-04-26 13:51:15 +0300

    import resolver packages installed and eslint and babel configured.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          // actions: './src/actions',
+          // api: './src/api',
+          // assets: './src/assets',
+          // reducers: './src/reducers',
+          // sagas: './src/sagas',
+          // translations: './src/translations',
+          components: './src/components',
+          screens: './src/screens',
+          theme: './src/theme.js',
+          // validations: './src/validations',
+          // _sizes: './src/_sizes',
+          // _globalStyles: './src/_globalStyles',
+          // _environment: './src/_environment',
+        },
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.json'],
+        alias: {
+          // actions: './src/actions',
+          // api: './src/api',
+          // assets: './src/assets',
+          // reducers: './src/reducers',
+          // sagas: './src/sagas',
+          // translations: './src/translations',
+          components: './src/components',
+          screens: './src/screens',
+          theme: './src/theme.js',
+          // validations: './src/validations',
+          // _sizes: './src/_sizes',
+          // _globalStyles: './src/_globalStyles',
+          // _environment: './src/_environment',
+        },
+      },
+    ],
+  ],
 };
