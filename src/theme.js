878e86db98093c8e436370ecda5f59bcbf0e6b8a
tunahanoguz <tunahanog@gmail.com>
2020-04-26 14:01:51 +0300

    Colors created.

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,18 @@
-const colors = {};
+const colors = {
+  white: '#FFFFFF',
+  light: '#8799A3',
+  blue: '#1DA1F3',
+  darkBlue: '#1A6DA3',
+  barNavyBlue: '#131D27',
+  modalNavyBlue: '#16202A',
+  searchBoxNavyBlue: '#10171F',
+  divSBBorderGrey: '#39434D',
+  horizontalLineGrey: '#405362',
+  messageVBGrey: '#3D5466',
+  switchLightGrey: '#B9B9B9',
+  switchDarkGrey: '#5C636B',
+  pink: '#D6295E',
+};
 
 const spaces = [
   4,
