210070f19491242900b2ce356916a0fc3adda702
tunahanoguz <tunahanog@gmail.com>
2020-04-26 18:52:15 +0300

    User constans created.

---
+++ src/constants.js
@@ -0,0 +1,62 @@
+export const users = [
+  {
+    _id: 'userone',
+    fullName: 'Adrian G Witte',
+    username: 'adrianwitte',
+    profilePicture: require('./assets/images/avatar1.jpg'),
+  },
+  {
+    _id: 'usersecond',
+    fullName: 'Judy M Pagan',
+    username: 'judypagan',
+    profilePicture: require('./assets/images/avatar2.jpg'),
+  },
+  {
+    _id: 'userthree',
+    fullName: 'Eloise D Martinez',
+    username: 'eloisemartinez',
+    profilePicture: require('./assets/images/avatar3.jpg'),
+  },
+  {
+    _id: 'userfour',
+    fullName: 'Arnold M Waterman',
+    username: 'arnoldwaterman',
+    profilePicture: require('./assets/images/avatar4.jpg'),
+  },
+  {
+    _id: 'userfive',
+    fullName: 'Josephine E Wallace',
+    username: 'josephinewallace',
+    profilePicture: require('./assets/images/avatar5.jpg'),
+  },
+  {
+    _id: 'usersix',
+    fullName: 'Miriam C Price',
+    username: 'miriamprice',
+    profilePicture: require('./assets/images/avatar6.jpg'),
+  },
+  {
+    _id: 'userseven',
+    fullName: 'Edward M Campbell',
+    username: 'edwardcampbell',
+    profilePicture: require('./assets/images/avatar7.jpg'),
+  },
+  {
+    _id: 'usereight',
+    fullName: 'Sam R Roberson',
+    username: 'samroberson',
+    profilePicture: require('./assets/images/avatar8.jpg'),
+  },
+  {
+    _id: 'usernine',
+    fullName: 'John B Haley',
+    username: 'johnhaley',
+    profilePicture: require('./assets/images/avatar9.jpg'),
+  },
+  {
+    _id: 'userten',
+    fullName: 'Chris J Torres',
+    username: 'christorres',
+    profilePicture: require('./assets/images/avatar10.jpg'),
+  },
+];
