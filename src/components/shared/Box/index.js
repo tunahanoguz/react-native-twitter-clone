0eaf9486b54710854d8e42c2266519699ba58b30
tunahanoguz <tunahanog@gmail.com>
2020-04-26 13:52:10 +0300

    Box and Text components created.

---
+++ src/components/shared/Box/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { compose, flexbox, layout, space, color, border } from 'styled-system';
+
+const Box = styled.View(compose(flexbox, layout, space, color, border));
+
+export default Box;
---
+++ src/components/shared/Typography/index.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import {
+  compose,
+  flexbox,
+  typography,
+  space,
+  color,
+} from 'styled-system';
+import theme from '../../theme';
+
+function Text({ uppercase, children, ...props }) {
+  return (
+    <StyledText
+      style={uppercase && { textTransform: 'uppercase' }}
+      css={{ color: theme.colors.dark }}
+      {...props}>
+      {children}
+    </StyledText>
+  );
+}
+
+const StyledText = styled.Text(
+  compose(
+    flexbox,
+    typography,
+    space,
+    color,
+  ),
+);
+
+StyledText.defaultProps = {
+  variant: 'normal',
+};
+
+export default Text;
