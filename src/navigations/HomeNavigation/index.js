bd3f25200e01030fd7c1453a3dcb124f7a8f537e
tunahanoguz <tunahanog@gmail.com>
2020-04-26 14:53:40 +0300

    Updated header style in HomeNavigation.

--- src/navigations/HomeNavigation/index.js
+++ src/navigations/HomeNavigation/index.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from '../../screens';
+import theme from 'theme';
 
 function HomeNavigation() {
   const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.navyBlue,
+          borderBottomWidth: 1,
+          borderBottomColor: theme.colors.divSBBorderGrey,
+          elevation: 0,
+        },
+        headerTitleStyle: { color: theme.colors.white },
+      }}>
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
