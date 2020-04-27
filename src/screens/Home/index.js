2d7258b9d178a42e1282978abadca031c4eb8222
tunahanoguz <tunahanog@gmail.com>
2020-04-27 15:56:08 +0300

    Added Twitter component in Home screen to show tweets.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
+import { Box, Tweet } from 'components';
+import { users } from '../../constants';
 
 function Home() {
+  const data = [
+    {
+      _id: 'asdasdasd',
+      text:
+        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
+      images: [],
+    },
+    {
+      _id: 'asd',
+      text:
+        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
+      images: [],
+    },
+    {
+      _id: 'asda',
+      text:
+        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
+      images: [],
+    },
+  ];
+
   return (
-    <View>
-      <Text>Home page!</Text>
-    </View>
+    <Box flex={1}>
+      <FlatList
+        data={data}
+        renderItem={({ item, index }) => (
+          <Tweet user={users[index]} tweet={item} />
+        )}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <Box height={0.4} bg="divSBBorderGrey" />}
+        ListFooterComponent={() => (
+          <>
+            <Box height={0.4} bg="messageVBGrey" mb={7} />
+            <Box
+              alignSelf="center"
+              width={3}
+              height={3}
+              bg="light"
+              borderRadius={3}
+            />
+          </>
+        )}
+      />
+    </Box>
   );
 }
 
