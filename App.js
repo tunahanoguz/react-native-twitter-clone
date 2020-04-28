4332ebc180bcbae4179ab110a84ee6d09cd1674e
tunahanoguz <tunahanog@gmail.com>
2020-04-28 11:58:15 +0300

    Created splash screen.

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { RootNavigation } from './src/navigations';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 
 function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor={theme.colors.barNavyBlue} />
