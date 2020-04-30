9f370b9f3eb17164410e278acecf9524ac77989a
tunahanoguz <tunahanog@gmail.com>
2020-04-30 16:13:37 +0300

    Icon component created.

--- src/components/shared/Icon/index.js
+++ src/components/shared/Icon/index.js
@@ -1,7 +1,7 @@
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import styled from 'styled-components';
-import { compose, typography, space, color } from 'styled-system';
+import { compose, position, typography, space, color } from 'styled-system';
 
-const Icon = styled(FeatherIcon)(compose(typography, space, color));
+const Icon = styled(FeatherIcon)(compose(position, typography, space, color));
 
 export default Icon;
