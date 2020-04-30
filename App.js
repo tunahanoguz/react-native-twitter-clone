3f91d314e67e5fa99766db7b7c526408278d8d58
tunahanoguz <tunahanog@gmail.com>
2020-04-30 16:16:44 +0300

    Wrapped navigation by SafeAreaProvider.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
 import { RootNavigation } from './src/navigations';
 import { ThemeProvider } from 'styled-components';
@@ -15,9 +16,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor={theme.colors.barNavyBlue} />
-      <NavigationContainer>
-        <RootNavigation />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
