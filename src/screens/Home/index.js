5cf4b89105e4e59e616cb237dcfd72d51e9a0c86
tunahanoguz <tunahanog@gmail.com>
2020-04-30 16:12:23 +0300

    New tweets added and the content wrapped by SafeAreaView

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import { Box, Tweet } from 'components';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Box, Tweet, NewTweetButton } from 'components';
 import { users } from '../../constants';
 
 function Home() {
@@ -28,30 +29,49 @@ function Home() {
         'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
       images: [],
     },
+    {
+      _id: 'asdaa',
+      text:
+        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
+      images: [],
+    },
+    {
+      _id: 'asdaaa',
+      text:
+        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
+      images: [],
+    },
   ];
 
   return (
     <Box flex={1}>
-      <FlatList
-        data={data}
-        renderItem={({ item, index }) => (
-          <Tweet user={users[index]} tweet={item} />
-        )}
-        keyExtractor={(item) => item._id}
-        ItemSeparatorComponent={() => <Box height={0.4} bg="divSBBorderGrey" />}
-        ListFooterComponent={() => (
-          <>
-            <Box height={0.4} bg="messageVBGrey" mb={7} />
-            <Box
-              alignSelf="center"
-              width={3}
-              height={3}
-              bg="light"
-              borderRadius={3}
-            />
-          </>
-        )}
-      />
+      <SafeAreaView>
+        <FlatList
+          data={data}
+          renderItem={({ item, index }) => (
+            <Tweet user={users[index]} tweet={item} />
+          )}
+          keyExtractor={(item) => item._id}
+          ItemSeparatorComponent={() => (
+            <Box height={0.4} bg="divSBBorderGrey" />
+          )}
+          ListFooterComponent={() => (
+            <>
+              <Box height={0.4} bg="messageVBGrey" mb={7} />
+              <Box
+                alignSelf="center"
+                width={3}
+                height={3}
+                bg="light"
+                borderRadius={3}
+              />
+            </>
+          )}
+        />
+
+      </SafeAreaView>
+
+      <NewTweetButton />
     </Box>
   );
 }
