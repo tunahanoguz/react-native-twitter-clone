f7b5b46cbfb385d0c48631bb2183dea9a62ca40f
tunahanoguz <tunahanog@gmail.com>
2020-05-01 12:29:53 +0300

    tweets constants removed

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,52 +2,14 @@ import React from 'react';
 import { FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Box, Tweet, NewTweetButton } from 'components';
-import { users } from '../../constants';
+import { tweets, users } from '../../constants';
 
 function Home() {
-  const data = [
-    {
-      _id: 'asdasdasd',
-      text:
-        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
-      images: [
-        require('../../assets/images/image1.jpg'),
-        // require('../../assets/images/image2.jpg'),
-        // require('../../assets/images/image3.jpg'),
-        // require('../../assets/images/image4.jpg'),
-      ],
-    },
-    {
-      _id: 'asd',
-      text:
-        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
-      images: [],
-    },
-    {
-      _id: 'asda',
-      text:
-        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
-      images: [],
-    },
-    {
-      _id: 'asdaa',
-      text:
-        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
-      images: [],
-    },
-    {
-      _id: 'asdaaa',
-      text:
-        'In hospitals across the Gulf foreigners are on the front the fighting covid-19. Nevertheless, the pandemic has revealed more bigotry than benevolence',
-      images: [],
-    },
-  ];
-
   return (
     <Box flex={1}>
       <SafeAreaView>
         <FlatList
-          data={data}
+          data={tweets}
           renderItem={({ item, index }) => (
             <Tweet user={users[index]} tweet={item} />
           )}
@@ -68,7 +30,6 @@ function Home() {
             </>
           )}
         />
-
       </SafeAreaView>
 
       <NewTweetButton />
